d986e6021853c73fff909ae595b09d5579683a64
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-02-04 20:26:30 +0500

    components full

---
+++ src/components/heroBanner/HeroBanner.jsx
@@ -0,0 +1,5 @@
+import "./HeroBanner.css";
+const HeroBanner = () => {
+  return <div className="heroBanner">HeroBanner</div>;
+};
+export default HeroBanner;
---
+++ src/components/tourist/Tourist.jsx
@@ -0,0 +1,5 @@
+import "./Tourist.css";
+const Tourist = () => {
+  return <div className="tourist">Tourist</div>;
+};
+export default Tourist;

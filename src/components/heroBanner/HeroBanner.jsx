e2678088716e69f7795267aaf9b273553e33d186
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-02-05 17:42:57 +0500

    hero banner media full

--- src/components/heroBanner/HeroBanner.jsx
+++ src/components/heroBanner/HeroBanner.jsx
@@ -1,5 +1,41 @@
 import "./HeroBanner.css";
+import HeroBannerImg from "../../assets/images/heroBanner.png";
+import { MdKeyboardArrowDown } from "react-icons/md";
 const HeroBanner = () => {
-  return <div className="heroBanner">HeroBanner</div>;
+  return (
+    <div className="hero">
+      <img className="hero_img" src={HeroBannerImg} alt="heroBannerImg" />
+      <div className="heroBannerBottom">
+        <div className="heroBottom_card">
+          <p className="heroBottom_title">Gender</p>
+          <p className="heroBottom_text">
+            Male <MdKeyboardArrowDown />
+          </p>
+        </div>
+        <p className="heroBottom_linr"></p>
+        <div className="heroBottom_card">
+          <p className="heroBottom_title">Age</p>
+          <p className="heroBottom_text">
+            24+ <MdKeyboardArrowDown />
+          </p>
+        </div>
+        <p className="heroBottom_linr"></p>
+        <div className="heroBottom_card">
+          <p className="heroBottom_title">Language</p>
+          <p className="heroBottom_text">
+            English <MdKeyboardArrowDown />
+          </p>
+        </div>
+        <p className="heroBottom_linr"></p>
+        <div className="heroBottom_card">
+          <p className="heroBottom_title">Lacalisation</p>
+          <p className="heroBottom_text">
+            Cairo <MdKeyboardArrowDown />
+          </p>
+        </div>
+        <button className="heroBottom_btn">Search</button>
+      </div>
+    </div>
+  );
 };
 export default HeroBanner;
